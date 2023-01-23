b8bdaa6adff315bd79f5eace951f9346baae9e7d
Jonyelson Lopes <jonyelson20@gmail.com>
2023-01-23 11:41:07 -0300

    chore: created application routes

---
+++ src/router.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+
+export const router = Router();
+
+router.get('/categories', (req, res) => {
+  res.send('OK');
+});
+
+router.get('/products', (req, res) => {
+  res.send('OK');
+});
+
+router.post('/products', (req, res) => {
+  res.send('OK');
+});
+
+router.get('/products/:categoryId/products', (req, res) => {
+  res.send('OK');
+});
+
+router.get('/orders', (req, res) => {
+  res.send('OK');
+});
+
+router.post('/orders', (req, res) => {
+  res.send('OK');
+});
+
+router.patch('/orders/:orderId', (req, res) => {
+  res.send('OK');
+});
+
+router.delete('/orders/:orderId', (req, res) => {
+  res.send('OK');
+});
